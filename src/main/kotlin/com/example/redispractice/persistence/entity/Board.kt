1206d7e86bed5d819e6d8c1798cbe00cb34a65df
lgm1007 <flxl654@gmail.com>
2024-09-09 19:52:50 +0900

    modify: Entity 내부 필드로 & private set으로 setter 막기

--- src/main/kotlin/com/example/redispractice/persistence/entity/Board.kt
+++ src/main/kotlin/com/example/redispractice/persistence/entity/Board.kt
@@ -5,12 +5,9 @@ import org.springframework.data.redis.core.RedisHash
 
 @RedisHash("Boards")
 class Board(
-    @Id val id: String? = null,
     var title: String,
     var author: String,
 ) {
-    constructor(title: String, author: String) : this(null, title, author) {
-        this.title = title
-        this.author = author
-    }
+    @Id var id: String? = null
+        private set
 }
