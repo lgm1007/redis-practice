573113b79f3adb831ac9c7152f8db302b4906a94
lgm1007 <flxl654@gmail.com>
2024-09-11 11:43:54 +0900

    test: RestTemplate를 사용한 저장, 조회 기능 테스트

--- src/main/kotlin/com/example/redispractice/config/RedisConfig.kt
+++ src/main/kotlin/com/example/redispractice/config/RedisConfig.kt
@@ -2,12 +2,14 @@ package com.example.redispractice.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
+@Configuration
 class RedisConfig {
     @Value("\${spring.redis.host}")
     private lateinit var redisHost: String
--- src/test/kotlin/com/example/redispractice/service/BoardServiceTest.kt
+++ src/test/kotlin/com/example/redispractice/service/BoardServiceTest.kt
@@ -7,11 +7,14 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.redis.core.RedisTemplate
+import kotlin.test.assertEquals
 
 @SpringBootTest
 class BoardServiceTest(
     @Autowired var boardService: BoardService,
     @Autowired var boardRepository: BoardRepository,
+    @Autowired var redisTemplate: RedisTemplate<String, Any>,
 ) {
 
     @BeforeEach
@@ -28,5 +31,15 @@ class BoardServiceTest(
 
     private fun createBoard(): Board {
         return Board(title = "게시글", author = "홍길동")
+    }
+
+    @Test
+    fun redisTemplate_save() {
+        val valueOperations = redisTemplate.opsForValue()
+
+        valueOperations.set("string_key", "value_1")
+
+        val value = valueOperations.get("string_key")
+        assertEquals("value_1", value)
     }
 }
